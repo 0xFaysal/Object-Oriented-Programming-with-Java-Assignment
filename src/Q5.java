d5a353d0309efea81427dad34e4e6628fa00eecf
Faysal Ahmed Fahim <ffahim2330717@bscse.uiu.ac.bd>
2024-10-21 12:51:20 +0600

    Question solve 1 to 5

--- src/Q5.java
+++ src/Q5.java
@@ -1,7 +1,38 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 
 class GUITest {
      public static void main(String[] args) {
          JFrame f = new JFrame("Oceans App");
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setSize(300, 150);
+            f.setLayout(new FlowLayout());
+
+            String [] oceans = {"Pacific", "Atlantic", "Indian", "Arctic", "Antarctic"};
+
+            JTextField tf1 = new JTextField(20);
+            JTextField tf2 = new JTextField(20);
+
+            //add code here
+            JButton b = new JButton("Show");
+            b.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    int index = Integer.parseInt(tf1.getText());
+                    tf2.setText(oceans[index]);
+                }
+            });
+
+
+            f.add(tf1);
+            f.add(tf2);
+            f.add(b);
+
+
+            f.setVisible(true);
      }
 }
